0e4c1df76562f51d824d7cb3e7d9be76d2c498e3
Pawel Wujczyk <pwujczyk@hotmail.com>
2022-02-27 06:42:44 +0100

    datastore

--- datastore.js
+++ datastore.js
@@ -1,14 +1,32 @@
-async function getTask() {
+async function getName() {
     const { Datastore } = require('@google-cloud/datastore');
     const datastore = new Datastore();
     const query = datastore.createQuery('Names');
-    console.log(datastore);
+    //console.log(datastore);
     const result = await datastore.runQuery(query);
     console.log(result);
 }
 
-async function addTask(){
-    
+async function addName() {
+    const { Datastore } = require('@google-cloud/datastore');
+    const datastore = new Datastore();
+
+    const k = datastore.key('Names')
+    console.log(k);
+    const entity = {
+        key: k,
+        data: [
+            {
+                name: 'Marcin',
+            },
+        ]
+    };
+    console.log(entity);
+    await datastore.save(entity);
+
 }
 
-getTask()
+
+//getName()
+addName().catch(console.error); 
+//getName();
